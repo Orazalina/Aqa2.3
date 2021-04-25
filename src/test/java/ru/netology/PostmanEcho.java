deda36fb7118fc197f5fa1be3e2c144e72c8253d
Anara Orazalina <orazalina.anara@inbox.ru>
2021-04-26 01:18:12 +0600

    returned the test back

--- src/test/java/ru/netology/PostmanEcho.java
+++ src/test/java/ru/netology/PostmanEcho.java
@@ -20,7 +20,7 @@ public class PostmanEcho {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Привет Hi"))
+                .body("data", equalTo("Привет"))
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
         ;
 
